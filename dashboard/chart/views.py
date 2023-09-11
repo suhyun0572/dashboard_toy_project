4249b0ae0c4884075d489a79018752fa4538ae9a
suhyun0572 <suhyun0572@gmail.com>
2023-09-11 15:09:33 +0200

    done : model , form , template / but not working..

--- dashboard/chart/views.py
+++ dashboard/chart/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TrailerForm
 
 # Create your views here.
 
 
 def test(request):
     return render(request,'chart.html',{'test':123})
+
+def trailerAdd(request):
+    print("test")
+    print(request)
+    if request.method == 'POST':
+        form = TrailerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('test')
+        else:
+            return redirect('test')
+    else:
+        form = TrailerForm()
+        return render(request, 'trailerAdd', {'form':form})
